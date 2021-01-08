2e6bece057ae59fed8c03ee7d3286a18e69f1c96
Joakim Teixeira <setokim1@hotmail.com>
2021-01-08 08:31:24 -0300

    Add initial files

---
+++ backend/src/main/java/com/slashdev/slashdeliver/SlashdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.slashdev.slashdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlashdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlashdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/slashdev/slashdeliver/SlashdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slashdev.slashdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlashdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
