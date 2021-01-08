db5bbf2aea92df4038e8e11b713b2ae0266a9e19
Joakim Teixeira <setokim1@hotmail.com>
2021-01-08 12:14:24 -0300

    Add product service layer

---
+++ backend/src/main/java/com/slashdev/slashdeliver/services/ProductService.java
@@ -0,0 +1,9 @@
+package com.slashdev.slashdeliver.services;
+
+import org.springframework.stereotype.Service;
+
+// Annotation to register component in Spring Boot
+@Service
+public class ProductService {
+
+}
