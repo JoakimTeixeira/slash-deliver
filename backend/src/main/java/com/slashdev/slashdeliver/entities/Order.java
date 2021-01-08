b04ca6696667847dd68ad2a760445eb2f4bd3da4
Joakim Teixeira <setokim1@hotmail.com>
2021-01-08 10:06:45 -0300

    Add annotations for JPA ORM

--- backend/src/main/java/com/slashdev/slashdeliver/entities/Order.java
+++ backend/src/main/java/com/slashdev/slashdeliver/entities/Order.java
@@ -5,10 +5,25 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+//Annotations for ORM (JPA)
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+//	Annotations for ID with auto-increment
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String address;
 	private Double latitude;
@@ -19,6 +34,11 @@ public class Order implements Serializable {
 	private OrderStatus status;
 	
 //	Set collection -> prevents fields repetition
+	@ManyToMany
+	@JoinTable(name = "tb_order_product",
+		joinColumns = @JoinColumn(name = "order_id"),
+		inverseJoinColumns = @JoinColumn(name = "product_id")
+			)
 	private Set<Product> products = new HashSet<>();
 	
 	public Order() {
