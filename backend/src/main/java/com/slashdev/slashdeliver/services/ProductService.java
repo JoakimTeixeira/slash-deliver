24d03a3ea4d509436fed0ad41b37c87f9b9fe80f
Joakim Teixeira <setokim1@hotmail.com>
2021-01-08 13:00:10 -0300

    Implement product service layer logic

--- backend/src/main/java/com/slashdev/slashdeliver/services/ProductService.java
+++ backend/src/main/java/com/slashdev/slashdeliver/services/ProductService.java
@@ -1,9 +1,29 @@
 package com.slashdev.slashdeliver.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.slashdev.slashdeliver.dto.ProductDTO;
+import com.slashdev.slashdeliver.entities.Product;
+import com.slashdev.slashdeliver.repositories.ProductRepository;
 
 // Annotation to register component in Spring Boot
 @Service
 public class ProductService {
-
+	
+//	Applies dependency injection for class constructor
+	@Autowired
+	private ProductRepository repository;
+	
+//	Transform Product items to ProductDTO items, using lambda expressions
+//	Annotation to prevent write lock in the database (increases transaction speed)
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll() {
+		List<Product> list = repository.findAll();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
 }
