68df1fafebe222f584ea63bbd28378af7d0c8639
Joakim Teixeira <setokim1@hotmail.com>
2021-01-21 18:18:45 -0300

    Add front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Slash Delivery front-web
+      </div>
+    </div>
+  );
+}
+
+export default App;
